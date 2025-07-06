ed75fe7600274f1d81da6a154a64f999f07981ae
Bárbara Arruda <65787282+barbaraarruda@users.noreply.github.com>
2025-07-06 19:10:29 -0300

    ordem de precedência

---
+++ conceitos_basicos/oo_precedencia.py
@@ -0,0 +1,18 @@
+#ordem de precedência 
+
+#quando se coloca os parênteses, indicamos ao código o que vem primeiro. Diferente da matemática que a multiplicação vem primeiro
+calculo = (2+5)*5
+print(calculo)
+
+calculoSec = (2+5) * ((5+4)*3)
+print(calculoSec)
+
+#operadores relacionais 
+"""
+== : igual - comparar se dois números são iguais
+> : maior
+< : menor
+>= : maior igual
+<= : menor igual
+!= : diferente
+"""
