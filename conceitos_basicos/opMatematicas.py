6ad6bc0b4b3a1f5fba74766448681bc9a26f0561
Bárbara Arruda <65787282+barbaraarruda@users.noreply.github.com>
2025-07-06 18:38:34 -0300

    operações matemáticas

---
+++ conceitos_basicos/opMatematicas.py
@@ -0,0 +1,60 @@
+#operações matemáticas
+"""
+soma = +
+subtração -
+multiplicação *
+divisão /
+divisão inteira //
+"""
+
+#soma 
+"""
+numero1 = int(input('digite o número 01: '))
+numero2 = int(input('digite o número 02: '))
+soma = numero1 + numero2
+print(soma)
+"""
+
+#subtração
+"""
+numero1 = int(input('digite o número 01: '))
+numero2 = int(input('digite o número 02: '))
+subtrair = numero1 - numero2
+print(subtrair)
+"""
+
+#multiplicação
+"""
+numero1 = int(input('digite o número 01: '))
+numero2 = int(input('digite o número 02: '))
+multiplicar = numero1 * numero2
+print(multiplicar)
+"""
+
+#divisão.  -> a divisão sempre vai retornar uma casa após a vírgula
+"""
+numero1 = int(input('digite o número 01: '))
+numero2 = int(input('digite o número 02: '))
+dividir = numero1 / numero2
+print(dividir)
+"""
+
+#divisão inteira. -> não retorna casas decimais.
+"""
+numero1 = int(input('digite o número 01: '))
+numero2 = int(input('digite o número 02: '))
+divInteira = numero1 // numero2
+print(divInteira)
+"""
+
+#incremento
+"""valor = 5
+valor += 1
+print(valor)"""
+
+#decremento
+"""valor = 5
+valor -= 1
+print(valor)"""
+
+#resto
