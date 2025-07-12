94b8bc6d39f26d0758c9d210c4c1e0ac243436e4
Bárbara Arruda <65787282+barbaraarruda@users.noreply.github.com>
2025-07-12 18:11:55 -0300

    encerramento primeiro módulo

---
+++ 01format.py
@@ -0,0 +1,4 @@
+valor = 45.3458328
+print(f'{valor:.2f}')
+
+print('Olá {}, você tem {} anos'.format('Bárbara', 27))
---
+++ conceitos_basicos/formatacao_mensagens.py
@@ -0,0 +1,6 @@
+#formatação de mensagens
+
+numero1 = int(input('Digite o número 01: '))
+numero2 = int(input('Digite o número 02: '))
+soma = numero1 + numero2
+print(f'A soma dos números digitados é: {soma}') #colocamos variáveis e strings no mesmo print2
