32eeb6330501bbd920bbb529c535b886b093009c
Bárbara Arruda <65787282+barbaraarruda@users.noreply.github.com>
2025-07-13 14:49:15 -0300

    módulo de decisão e repetição completo

---
+++ decisao_e_repeticao/for.py
@@ -0,0 +1,5 @@
+#loops infinitos, ele executa o mesmo trecho de código para cada item.
+
+frutas = ['maçã', 'banana', 'uva'] #lista 
+for fruta in frutas:
+    print(fruta)
---
+++ decisao_e_repeticao/if_else.py
@@ -0,0 +1,5 @@
+numero = int(input('Digite um número: '))
+if numero > 0:
+    print('Número é positivo')
+else:
+    print('Número é negativo')
